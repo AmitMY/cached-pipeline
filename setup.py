b835e5eb4dbece0e49d0f170e0314f76bf2aff71
Amit Moryossef <amitmoryossef@gmail.com>
2019-07-01 12:35:35 +0200

    chore(release): setup first time

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Amit Moryossef',
     author_email='amitmoryossef@gmail.com',
     url='https://github.com/AmitMY/cached-pipeline',
-    download_url='https://github.com/AmitMY/cached-pipeline/archive/0.1.tar.gz',
+    download_url='https://github.com/AmitMY/cached-pipeline/archive/0.2.tar.gz',
     keywords=['pipeline'],  # Keywords that define your package best
     install_requires=[
         'numpy',
